71923a9a6acc517171a84e41479a776db5fe23f3
Egger Haaaaan <bboyhan@hotmail.com>
2020-02-04 08:11:39 -0500

    Update git.sh

--- swarm/git/git.sh
+++ swarm/git/git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This function can be added to your ~/.bashrc or ~/.profile or ~/.bash_profile
+# or you can copy this script into your ~/bin/ folder
 
 function git () {
     (docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git nahragde/git "$@")

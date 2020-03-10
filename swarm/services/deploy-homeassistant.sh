9fabc063493aa2606dc9ece31cd74b1515e710d4
Edgar Han <bboyhan@hotmail.com>
2020-03-09 23:24:52 -0400

    homeassistant ready

---
+++ swarm/services/deploy-homeassistant.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Home-Assistant
+
+#docker run --init -d \
+#	--name="home-assistant" \
+#	-e "TZ=America/New_York" \
+#	-v /PATH_TO_YOUR_CONFIG:/config \
+#	--net=host \
+#      	homeassistant/home-assistant:stable
+
+docker service create \
+  --name "home-assistant" \
+  --env TZ="America/New_York" \
+  --publish=8123:8123/tcp \
+  homeassistant/home-assistant
+
+
+#  --mount=type=bind,src=config,dst=/config \
